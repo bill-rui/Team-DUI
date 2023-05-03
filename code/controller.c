266bf6d762e2e5921d2a2214c93178858e6fb4dd
gtzook <43184112+gtzook@users.noreply.github.com>
2023-05-02 22:35:55 -0500

    Update controller.c

--- code/controller.c
+++ code/controller.c
@@ -5,11 +5,11 @@
 #include <stdbool.h>
 #include <stdint.h>
 #define BUTTON_MASK (BIT1 + BIT2) // Port 1
-void ConfigureAdc();
-unsigned int ADC_Result;
-unsigned int counter = 0;
-volatile bool drive_en = false;
-volatile bool button = false;
+void ConfigureAdc(); // Init ADC
+unsigned int ADC_Result; // Store ADC value
+unsigned int counter = 0; // Number of "drunk" frames detected
+volatile bool drive_en = false; // Is driving enabled?
+volatile bool button = false; // State of button
 void button_init()
 {
     // Switches are pins 1.1, 1.2. They pull low when depressed.
@@ -28,7 +28,7 @@ int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
-    __bis_SR_register(SCG0);
+    __bis_SR_register(SCG0); // Setup clock for ADC sampling
     CSCTL3 |= SELREF__REFOCLK;
     CSCTL0 = 0;
     CSCTL1 &= ~(DCORSEL_7);
@@ -36,7 +36,7 @@ int main(void)
     CSCTL2 = FLLD_0 + 243;
     __delay_cycles(3);
     __bic_SR_register(SCG0);
-
+    // Wait for setup to finish
     while (CSCTL7 & (FLLUNLOCK0 | FLLUNLOCK1))
         ;
     CSCTL4 = SELMS__DCOCLKDIV | SELA__REFOCLK;
@@ -45,45 +45,45 @@ int main(void)
     UCA0CTLW0 |= UCSSEL__SMCLK;
     UCA0BR0 = 52;
     UCA0MCTLW = 0x4900 | UCOS16 | UCBRF1;
-
+    // Set 1.4 and 1.5 to UART function
     P1SEL1 &= ~BIT5 & ~BIT4;
     P1SEL0 |= BIT5 | BIT4;
-
+    // Set ADC input to A0
     SYSCFG2 = ADCPCTL0;
-
+    // Allow use of previously configured ports
     PM5CTL0 &= ~LOCKLPM5;
-
+    
     UCA0CTLW0 &= ~UCSWRST;
     UCA0IE |= UCRXIE;
-
+    
     button_init();
     ConfigureAdc();
-
+    // Init LED and set to low
     P2DIR |= BIT0;
     P2OUT &= ~BIT0;
-
+    // Send "standby" bit to RC car
     UCA0TXBUF = 'S';
     while (1)
     {
         ADCCTL0 |= ADCENC | ADCSC;
         __bis_SR_register(LPM0_bits + GIE); // go to sleep in low-power mode 0, all interrupts enabled
-        if (!drive_en)
+        if (!drive_en) // Driving not enabled
         {
-            if (ADC_Result < 500)
+            if (ADC_Result < 500) // Check if "drunk" is detected
                 counter++;
             else
-                counter = 0;
+                counter = 0; // Reset if it was noise
         }
-        if (counter > 20)
+        if (counter > 20) // Counter is past threshold
         {
-            drive_en = true;
-            P2OUT |= BIT0;
-            UCA0TXBUF = 'O';
+            drive_en = true; // Enable driving
+            P2OUT |= BIT0; // Turn on LED
+            UCA0TXBUF = 'O'; // Send "drive mode" to RC car
         }
     }
     return 0;
 }
-
+// UART interrupt
 #pragma vector = USCI_A0_VECTOR
 __interrupt void USCI_A0_ISR(void)
 {
@@ -102,6 +102,7 @@ __interrupt void USCI_A0_ISR(void)
         break;
     }
 }
+// Button Interrupt 
 #pragma vector=PORT1_VECTOR
 __interrupt void button_vector(void)
 {
@@ -111,11 +112,11 @@ __interrupt void button_vector(void)
         P1IFG &= ~BIT1; //Clear IFG register
         if (button)
         {
-            UCA0TXBUF = 'r';
+            UCA0TXBUF = 'r'; // Send "stop" to car
         }
         else
         {
-            UCA0TXBUF = 'R';
+            UCA0TXBUF = 'R'; // Send "go" to car
         }
         button = !button;
 
@@ -148,7 +149,7 @@ __interrupt void ADC_ISR(void)
     case ADCIV_ADCLOIFG:
         break;
     case ADCIV_ADCIFG:
-        ADC_Result = ADCMEM0;
+        ADC_Result = ADCMEM0; // Update ADC result
         __bic_SR_register_on_exit(LPM0_bits);
         break;
     default:
